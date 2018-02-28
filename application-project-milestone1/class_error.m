6f3d3f705c9e7329c1c62f1ec49d2b103a25f66c
Wilson Wang <wilson.wang@wustl.edu>
2018-02-27 20:29:23 -0600

    kk

--- application-project-milestone1/class_error.m
+++
@@ -1,9 +0,0 @@
-function [ error ] = class_error( w,X,y )
-    n=size(X,1);
-    col=size(X,2)+1;
-    xte(:,2:col)=X;
-    xte(:,1)=1;
-    preds=sign(xte*w');
-    error=sum(abs(y-preds)/2)/n;
-end
-
